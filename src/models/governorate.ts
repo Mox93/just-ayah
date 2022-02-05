1087618344fb9d1a6352645f0bed376ac5085123
mox93 <mohamed.ragaiy.saleh@gmail.com>
2022-02-05 17:28:31 +0200

    Implemented governorate selector

--- src/models/governorate.ts
+++ src/models/governorate.ts
@@ -1,31 +1,42 @@
 export const governorate = [
-  "Alexandria",
-  "Aswan",
-  "Asyut",
-  "Beheira",
-  "BeniSuef",
   "Cairo",
-  "Dakahlia",
-  "Damietta",
-  "Faiyum",
-  "Gharbia",
-  "Giza",
-  "Ismailia",
-  "KafrElSheikh",
-  "Luxor",
-  "Matruh",
-  "Minya",
+  "Alexandria",
   "Monufia",
-  "NewValley",
-  "NorthSinai",
-  "PortSaid",
+  "Gharbia",
+  "Dakahlia",
   "Qalyubia",
-  "Qena",
-  "RedSea",
-  "Sharqia",
+  "Ismailia",
+  "Faiyum",
+  "BeniSuef",
   "Sohag",
-  "SouthSinai",
+  "Asyut",
+  "Qena",
+  "Minya",
+  "Luxor",
+  "Aswan",
+  "Matruh",
+  "PortSaid",
+  "Damietta",
+  "RedSea",
+  "Giza",
+  "KafrElSheikh",
+  "NewValley",
+  "Beheira",
+  "Sharqia",
   "Suez",
+  "NorthSinai",
+  "SouthSinai",
 ] as const;
 
 export type Governorate = typeof governorate[number];
+
+export const egPrefix = "EG.";
+export const egStrip = (value: string) => value.replace(egPrefix, "");
+
+export const governorateList = governorate.map(
+  (element) => `${egPrefix}${element}`
+);
+
+export const handleEgGov = (value: string, g: (value: string) => string) => {
+  return value.startsWith(egPrefix) ? g(egStrip(value)) : value;
+};
