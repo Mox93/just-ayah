2a84c2795e465f507bc435fd2ad1315c7b46e20e
mox93 <mohamed.ragaiy.saleh@gmail.com>
2022-06-06 22:54:41 +0200

    Split Container to header body and footer

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,18 +1,45 @@
-import { forwardRef, PropsWithChildren, Ref } from "react";
+import { forwardRef, PropsWithChildren, ReactNode, Ref } from "react";
+
+import { useDirT } from "hooks";
 import { cn } from "utils";
 
 interface ContainerProps {
   className?: string;
   variant?: "menu" | "card";
+  dir?: string;
+  header?: ReactNode;
+  footer?: ReactNode;
 }
 
 const Container = (
-  { className, children, variant }: PropsWithChildren<ContainerProps>,
+  {
+    className,
+    children,
+    variant,
+    dir,
+    header,
+    footer,
+  }: PropsWithChildren<ContainerProps>,
   ref: Ref<HTMLDivElement>
 ) => {
+  const dirT = useDirT();
+  const _dir = dir || dirT;
+
   return (
-    <div ref={ref} className={cn("Container", variant, className)}>
-      {children}
+    <div ref={ref} className={cn("Container", variant, className)} dir={_dir}>
+      {header && (
+        <div className="header" dir={_dir}>
+          {header}
+        </div>
+      )}
+      <div className="body" dir={_dir}>
+        {children}
+      </div>
+      {footer && (
+        <div className="footer" dir={_dir}>
+          {footer}
+        </div>
+      )}
     </div>
   );
 };
