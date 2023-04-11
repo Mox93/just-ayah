43facebc52f00eb4fbf78f4b34e8d94a7686bdfd
mox93 <mohamed.ragaiy.saleh@gmail.com>
2023-04-11 02:39:12 +0200

    Moved business login to context folder

--- functions/src/context/courses/index.ts
+++ functions/src/context/courses/index.ts
@@ -1,6 +1,6 @@
-import { COURSES_DOC_PATH, DOC_ID_CARD } from "../config";
-import { document } from "../lib";
-import { merge } from "../utils";
+import { COURSES_DOC_PATH, DOC_ID_CARD } from "../../config";
+import { document } from "../../lib";
+import { merge } from "../../utils";
 import { indexing } from "./indexing";
 
 export const onCourseDocWrite = document(COURSES_DOC_PATH(DOC_ID_CARD)).onWrite(
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,4 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
-export * from "./courses";
-export * from "./students";
-export * from "./teachers";
-export * from "./users";
+export * from "./context";
